5f20ff4855a70377e29586de04348d371df05566
kinziiee21 <kinzamanjiyani20@gmail.com>
2025-12-16 22:06:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ document.querySelectorAll(".primary-btn").forEach(button => {
         alert("Primary button clicked!");
     });
 });
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+    document.body.style.overflow =
+        navLinks.classList.contains("show") ? "hidden" : "auto";
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("show");
+        document.body.style.overflow = "auto";
+    });
+});
+
 
