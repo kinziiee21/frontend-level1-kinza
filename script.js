8b4dac95f775f0689904f27aa085ba228b0d5041
kinziiee21 <kinzamanjiyani20@gmail.com>
2025-12-16 21:40:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Frontend Internship Day 1 Loaded Successfully");
+document.querySelectorAll(".primary-btn").forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.disabled) return;
+        alert("Primary button clicked!");
+    });
+});
+
