f67de5bfeeb87ef743de5d23aa105f1693898f0d
kinziiee21 <kinzamanjiyani20@gmail.com>
2025-12-16 23:35:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,20 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 });
+const goTopBtn = document.getElementById("goTopBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        goTopBtn.style.display = "block";
+    } else {
+        goTopBtn.style.display = "none";
+    }
+});
+
+goTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
